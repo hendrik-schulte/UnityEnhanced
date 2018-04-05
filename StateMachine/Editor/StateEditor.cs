a00ba382e43cde5bfde68f2b2783a1c171bce177
Hendrik Schulte <dinah93@gmail.com>
2018-04-05 13:34:02 +0200

    Fixed serialization error

--- StateMachine/Editor/StateEditor.cs
+++ StateMachine/Editor/StateEditor.cs
@@ -8,28 +8,41 @@ namespace UE.StateMachine
     public class StateEditor : Editor
     {
         private SerializedProperty m_Script;
+        private SerializedProperty m_StateManager;
+        private SerializedProperty m_Description;
         
         private void OnEnable()
         {
             m_Script = serializedObject.FindProperty("m_Script");
+            m_StateManager = serializedObject.FindProperty("stateManager");
+            m_Description = serializedObject.FindProperty("DeveloperDescription");
         }
         
         public override void OnInspectorGUI()
         {
             var state = target as State;
+            
 
             GUI.enabled = false;
             EditorGUILayout.ObjectField(m_Script);
             GUI.enabled = true;
-            
+
             if (state.stateManager)
             {            
                 GUI.enabled = false;
                 EditorGUILayout.Toggle("Instanced", state.stateManager.Instanced);
                 GUI.enabled = true;
             }
+            
+            serializedObject.Update();
 
+//            EditorGUILayout.ObjectField(m_StateManager, typeof(StateManager));
+
+//            m_Description.stringValue = EditorGUILayout.TextArea(m_Description.stringValue);
+            
             DrawPropertiesExcluding(serializedObject, "m_Script");
+
+            serializedObject.ApplyModifiedProperties();
 
 
         	if (!state.stateManager) return;
