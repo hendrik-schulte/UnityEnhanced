045f5d49a014bde703a090a101dac4da909b0126
Hendrik Schulte <dinah93@gmail.com>
2018-04-17 22:42:04 +0200

    Added LoggingLevel options Added some icons to event and state assets

--- Common/Logging.cs
+++ Common/Logging.cs
@@ -8,7 +8,7 @@ namespace UE.Common
         {
             if (debugLog) Debug.Log(Identifier(sender) + msg);
         }
-        
+
         public static void Log(string sender, string msg, bool debugLog = true)
         {
             if (debugLog) Debug.Log(Brackets(sender) + msg);
@@ -18,7 +18,7 @@ namespace UE.Common
         {
             if (debugLog) Debug.LogWarning(Identifier(sender) + msg);
         }
-        
+
         public static void Warning(string sender, string msg, bool debugLog = true)
         {
             if (debugLog) Debug.LogWarning(Brackets(sender) + msg);
@@ -34,6 +34,34 @@ namespace UE.Common
             if (debugLog) Debug.LogError(Brackets(sender) + msg);
         }
 
+        /// <summary>
+        /// Formats and logs the given message to the unity log.
+        /// </summary>
+        /// <param name="sender">reference to the sender</param>
+        /// <param name="msg">the message</param>
+        /// <param name="messageLevel">LogLevel of this message.</param>
+        /// <param name="logLevel">LogLevel of this script</param>
+        public static void Log(object sender, string msg, LogLevel messageLevel, LogLevel logLevel)
+        {
+            if (messageLevel < logLevel) return;
+
+            var message = Identifier(sender) + msg;
+
+            switch (messageLevel)
+            {
+                case LogLevel.Verbose:
+                case LogLevel.Info:
+                    Debug.Log(message);
+                    break;
+                case LogLevel.Warning:
+                    Debug.LogWarning(message);
+                    break;
+                case LogLevel.Error:
+                    Debug.LogWarning(message);
+                    break;
+            }
+        }
+
         private static string Identifier(object sender)
         {
             return Brackets(sender.GetType().Name);
@@ -42,6 +70,14 @@ namespace UE.Common
         private static string Brackets(string value)
         {
             return "[" + value + "] ";
+        }
+
+        public enum LogLevel
+        {
+            Verbose = 0,
+            Info = 1,
+            Warning = 2,
+            Error = 3
         }
     }
 }
