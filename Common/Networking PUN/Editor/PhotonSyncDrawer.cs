6ecef156cbd229a5cf19c2f616dd568426d43d98
Hendrik Schulte <dinah93@gmail.com>
2018-07-07 15:26:45 +0200

    Fixed PhotonSync without asmdef

---
+++ Common/Networking PUN/Editor/PhotonSyncDrawer.cs
@@ -0,0 +1,46 @@
+﻿#if UNITY_EDITOR
+using UE.Common;
+using UnityEditor;
+using UnityEngine;
+
+namespace UE.PUNNetworking
+{
+    [CustomPropertyDrawer(typeof(PhotonSync))]
+    public class PhotonSyncDrawer : PropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            EditorGUI.BeginProperty(position, label, property);
+
+            var enabled = property.FindPropertyRelative("PUNSync");
+
+            EditorGUI.PropertyField(position.GetLine(1), enabled);
+
+            if (enabled.boolValue)
+            {
+                EditorGUI.indentLevel++;
+
+                EditorGUI.PropertyField(
+                    position.GetLine(2),
+                    property.FindPropertyRelative("cachingOptions"));
+
+                EditorGUI.indentLevel--;
+                
+            }
+
+            EditorGUI.EndProperty();
+        }
+
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            var lines = 1;
+
+            if (property.FindPropertyRelative("PUNSync").boolValue)
+                lines++;
+            
+                
+            return EditorUtil.PropertyHeight(lines);
+        }
+    }
+}
+#endif
--- Common/Networking PUN/PhotonSync.cs
+++ Common/Networking PUN/PhotonSync.cs
@@ -4,9 +4,6 @@ using UE.Common;
 using UE.Instancing;
 using UnityEngine;
 using Object = UnityEngine.Object;
-#if UNITY_EDITOR
-using UnityEditor;
-#endif
 
 namespace UE.PUNNetworking
 {
@@ -50,45 +47,5 @@ namespace UE.PUNNetworking
         }
 #endif
     }
-
-#if UNITY_EDITOR
-    [CustomPropertyDrawer(typeof(PhotonSync))]
-    public class PhotonSyncDrawer : PropertyDrawer
-    {
-        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
-        {
-            EditorGUI.BeginProperty(position, label, property);
-
-            var enabled = property.FindPropertyRelative("PUNSync");
-
-            EditorGUI.PropertyField(position.GetLine(1), enabled);
-
-            if (enabled.boolValue)
-            {
-                EditorGUI.indentLevel++;
-
-                EditorGUI.PropertyField(
-                    position.GetLine(2),
-                    property.FindPropertyRelative("cachingOptions"));
-
-                EditorGUI.indentLevel--;
-                
-            }
-
-            EditorGUI.EndProperty();
-        }
-
-        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
-        {
-            var lines = 1;
-
-            if (property.FindPropertyRelative("PUNSync").boolValue)
-                lines++;
-            
-                
-            return EditorUtil.PropertyHeight(lines);
-        }
-    }
-#endif
 }
 #endif
